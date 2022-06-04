af995e6bc3747785ba64afa60cc4e271825b1dfd
RoanDruziani <rddruziani@gmai.com>
2022-06-04 10:31:06 -0300

    api BackEnd

---
+++ models/Person.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Person = mongoose.model('Person', {
+    name: String,
+    email: String,
+    rua: String,
+    cidade: String,
+    estado: String,
+    approved: Boolean
+})
+
+module.exports = Person
